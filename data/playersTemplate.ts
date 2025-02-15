cb76ae5cfc5349e386765ff822b9b45ae28262f2
Bartek <bwatorek86@uczen.tl.krakow.pl>
2025-02-15 22:33:30 +0100

    player nicknames and colors template

---
+++ data/playersTemplate.ts
@@ -0,0 +1,26 @@
+export const playersTemplate = [
+  {
+    name: "Fred",
+    color: "#f00",
+  },
+  {
+    name: "Greenlee",
+    color: "#0f0",
+  },
+  {
+    name: "Pinkney",
+    color: "#f0f",
+  },
+  {
+    name: "Bluebell",
+    color: "#0ff",
+  },
+  {
+    name: "Willem",
+    color: "#f50",
+  },
+  {
+    name: "Greydon",
+    color: "#555",
+  },
+];
